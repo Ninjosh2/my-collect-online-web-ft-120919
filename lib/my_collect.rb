c1361e70624168bfb7ab422c07eaebea119d8781
Joshua Sanders <ninjosh2@gmail.com>
2019-12-17 01:16:15 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+list = ["Tim Jones", "Bob Costas", "Don Knotts"]
 
 
+def my_collect(array)
+  i = 0
+  names = []
+  while i < array.length
+    names << yield(array[i])
+  
+    i += 1
+  end
+  names
+end
+
+my_collect(list) {|i| i.split(" ").first}
